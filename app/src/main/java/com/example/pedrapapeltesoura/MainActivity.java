2906ba02bf77d9b2978b951a9140245588d24924
gabriel <gaamdal557@gmail.com>
2023-04-12 15:07:15 -0300

    parte 1

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selecionadoPedra(View v){
+        this.opcaoSelecionada("pedra");
+    }
+    public void selecionadoTesoura(View v){
+        this.opcaoSelecionada("tesoura");
+    }
+    public void selecionadoPapel(View v){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada){
+
+    }
+
 }

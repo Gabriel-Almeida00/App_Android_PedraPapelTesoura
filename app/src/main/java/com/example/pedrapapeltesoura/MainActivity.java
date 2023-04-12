7c0ced0f4326b2c48363a2068f63655320a7ae0e
gabriel <gaamdal557@gmail.com>
2023-04-12 15:42:04 -0300

    imagem aleatoria

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +28,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void opcaoSelecionada(String opcaoSelecionada){
 
+        ImageView imageResultado = findViewById(R.id.imageResultado);
+
+        String[]  opcoes = {"pedra", "tesoura", "pepel"};
+        int numero = new Random().nextInt(3);
+        String opcoesApp = opcoes[numero];
+
+        switch (opcoesApp){
+            case "pedra":
+                imageResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imageResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imageResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
     }
 
 }
